82b4a908640eddf88e7037b45ba0a8a8c0c54d8f
jinyongu <145449213+jinyongu@users.noreply.github.com>
2024-03-11 01:36:28 +0900

    Add files via upload

---
+++ learn-spring-framework-01/learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,25 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PackManGame;
+
+@Configuration
+public class GamingConfiguration {
+	
+	@Bean
+	public GamingConsole game() {
+		var game = new PackManGame();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+	
+}
